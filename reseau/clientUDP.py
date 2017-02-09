171258266bd243f444a5be83545e8231bfb51e35
sl4iut3 <sle0910.3@gmail.com>
2017-02-09 11:54:40 +0100

    Add files via upload

---
+++ reseau/clientUDP.py
@@ -0,0 +1,14 @@
+from socket import *
+
+serverName = '127.0.0.1'
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('data?')
+print message
+clientSocket.sendto(message,(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print modifiedMessage
+clientSocket.close()
+
+
