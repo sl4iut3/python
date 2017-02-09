30a91fd5a471b9c731cbe47d7b7353a38820d330
sl4iut3 <sle0910.3@gmail.com>
2017-02-09 12:01:44 +0100

    Update clientHTTP.py

--- reseau/clientHTTP.py
+++ reseau/clientHTTP.py
@@ -5,17 +5,23 @@ serverName = 'www.utt.fr'
 serverPort = 80
 
 page = '/fr/index.html'
+# socket TCP
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
+
 # message = raw_input('data?')
-message='GET '+page+' HTTP/1.1\nHost: '+serverName+'\n\n\n'
+requete = 'GET '+page+' HTTP/1.1\nHost: '+serverName+'\n\n\n'
 
 print message
 
+# envoi de la requete
 clientSocket.send(message)
 
+# recuperation et affichage de la reponse
 modifiedMessage = clientSocket.recv(8192)
 print modifiedMessage
+
+# fermeture
 clientSocket.close()
 
 
